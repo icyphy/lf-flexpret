4f378e001a75ee0e1bde4be339b79574e8a63c4f
xingyuli9961 <xingyuli9961@berkeley.edu>
2021-09-24 18:05:27 +0000

    unexpected repeating printings

--- examples/c/hello/hello.c
+++ examples/c/hello/hello.c
@@ -8,10 +8,11 @@ uint32_t add(uint32_t a, uint32_t b) {
 int main() {
     
     uint32_t x = 1;
+    _fp_print(x);
     uint32_t y = 2;
+    _fp_print(y);
     uint32_t z = add(x, y);
-
-    _fp_print(z);
+//    _fp_print(z);
 
     // Terminate the simulation.
     // Put a while loop to make sure no unwanted side effects.
