7f93b0a7c9a2ce38d8ff74d5bc61d7377bed1dbf
xingyuli9961 <xingyuli9961@berkeley.edu>
2021-09-25 21:50:09 +0000

    add sbrk1.0

--- examples/c/malloc/malloc.c
+++ examples/c/malloc/malloc.c
@@ -2,10 +2,30 @@
 #include <stdint.h>
 #include <flexpret_io.h>
 
-int main() {
+#include <_syslist.h>
 
+void *
+_sbrk (incr)
+     int incr;
+{
+   extern char   end; /* Set by linker.  */
+   static char * heap_end;
+   char *        prev_heap_end;
+
+   if (heap_end == 0)
+     heap_end = & end;
+
+   prev_heap_end = heap_end;
+   heap_end += incr;
+
+   return (void *) prev_heap_end;
+}
+
+int main() {
+    _fp_print(4);
     uint32_t* p = malloc(sizeof(uint32_t));
     *p = 100;
+    _fp_print(5);
     _fp_print(*p);
     free(p);
 
