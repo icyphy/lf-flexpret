ed5ec01ef21191c174f17100bb954114b2d5a1ff
Shaokai Lin <shaokai@berkeley.edu>
2022-02-23 15:57:27 -0800

    Remove legacy folders. They are now in the flexpret repo.

--- scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm *.vcd *txt
-ls | grep -v "\." | xargs rm
--- scripts/compile-and-parse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-SCRIPT_DIR=$(dirname "$0")
-
-$SCRIPT_DIR/compile.sh $1 $2
-$SCRIPT_DIR/parse_disasm.py $1.dump.txt readmemh > imem.hex.txt
