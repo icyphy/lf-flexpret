018e6228e531ba14eab8c1422edea88e133e56e6
Shaokai Lin <shaokai@berkeley.edu>
2022-03-09 11:22:27 -0800

    Add the no_cmake version

---
+++ lf/Minimal/src-gen-cpy/Minimal_no_cmake/core/platform/Platform.cmake
@@ -0,0 +1,4 @@
+# Check which system we are running on to select the correct platform support
+# file and assign the file's path to LF_PLATFORM_FILE
+# FIXME: Need a proper way to link this file.
+set(LF_PLATFORM_FILE ../../../../lf_embedded_flexpret_support.c)
---
+++ lf/Minimal/src-gen-cpy/Minimal_no_cmake/rebuild.sh
@@ -0,0 +1,2 @@
+riscv_clean.sh
+riscv_build.sh Minimal Minimal.c lf_embedded_flexpret_support.c ctarget.c  core/mixed_radix.c tinyalloc.c
