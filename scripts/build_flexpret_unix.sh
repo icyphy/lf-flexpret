c41ac3ebf89eafe06dcc33b4a765bc876f627dcf
Shaokai Lin <shaokai@berkeley.edu>
2022-11-10 00:16:38 -0800

    Attempt to fix a "bad array subscript" problem.

--- scripts/build_flexpret_unix.sh
+++ scripts/build_flexpret_unix.sh
@@ -7,21 +7,21 @@ echo "starting NRF generation script into $LF_SOURCE_GEN_DIRECTORY"
 echo "pwd is $(pwd)"
 
 # Parse filename from the src-gen directory name
-ARR=(${LF_SOURCE_GEN_DIRECTORY//\// })
-LF_FILENAME=${ARR[-1]} # Without the .lf extension
+IFS='/' read -ra ARR <<< "$LF_SOURCE_GEN_DIRECTORY"
+LF_FILENAME=${ARR[-1]} # LF filename without the .lf extension
 echo "LF filename is $LF_FILENAME.lf."
 
 # Copy c files into /core.
-cp $PROJECT_ROOT/flexpret/programs/lib/start.S $LF_SOURCE_GEN_DIRECTORY/core/
-cp $PROJECT_ROOT/flexpret/programs/lib/syscalls.c $LF_SOURCE_GEN_DIRECTORY/core/
-cp $PROJECT_ROOT/flexpret/programs/lib/startup.c $LF_SOURCE_GEN_DIRECTORY/core/
-cp -r $PROJECT_ROOT/flexpret/programs/lib/tinyalloc $LF_SOURCE_GEN_DIRECTORY/core/
-cp $PROJECT_ROOT/platform/lf_flexpret_support.c $LF_SOURCE_GEN_DIRECTORY/core/platform/
+cp "$PROJECT_ROOT/flexpret/programs/lib/start.S" "$LF_SOURCE_GEN_DIRECTORY/core/"
+cp "$PROJECT_ROOT/flexpret/programs/lib/syscalls.c" "$LF_SOURCE_GEN_DIRECTORY/core/"
+cp "$PROJECT_ROOT/flexpret/programs/lib/startup.c" "$LF_SOURCE_GEN_DIRECTORY/core/"
+cp -r "$PROJECT_ROOT/flexpret/programs/lib/tinyalloc" "$LF_SOURCE_GEN_DIRECTORY/core/"
+cp "$PROJECT_ROOT/platform/lf_flexpret_support.c" "$LF_SOURCE_GEN_DIRECTORY/core/platform/"
 
 # Copy header files into /include.
-cp -r $PROJECT_ROOT/flexpret/programs/lib/tinyalloc $LF_SOURCE_GEN_DIRECTORY/include/core/
-cp $PROJECT_ROOT/platform/lf_flexpret_support.h $LF_SOURCE_GEN_DIRECTORY/include/core/platform/
-cp $PROJECT_ROOT/platform/platform.h $LF_SOURCE_GEN_DIRECTORY/include/core/
+cp -r "$PROJECT_ROOT/flexpret/programs/lib/tinyalloc" "$LF_SOURCE_GEN_DIRECTORY/include/core/"
+cp "$PROJECT_ROOT/platform/lf_flexpret_support.h" "$LF_SOURCE_GEN_DIRECTORY/include/core/platform/"
+cp "$PROJECT_ROOT/platform/platform.h" "$LF_SOURCE_GEN_DIRECTORY/include/core/"
 
 printf '
 .DEFAULT_GOAL := all
@@ -29,30 +29,31 @@ printf '
 XLEN ?= 32
 
 # LF variables
+LF_PROJECT_ROOT := %s
 LF_SOURCE_GEN_DIRECTORY := %s
 LF_FILENAME := %s
 
 # FlexPRET scripts directory
-FP_C_SCRIPTS_DIR := $(LF_SOURCE_GEN_DIRECTORY)/../../flexpret/scripts/c/
+FP_C_SCRIPTS_DIR := "$(LF_PROJECT_ROOT)/flexpret/scripts/c/"
 
 # Compilation
 RISCV_PREFIX ?= riscv$(XLEN)-unknown-elf-
 RISCV_GCC ?= $(RISCV_PREFIX)gcc
 RISCV_GCC_OPTS ?= -g -static -O0 -march=rv32i -mabi=ilp32 -nostartfiles -specs=nosys.specs
-RISCV_LINK ?= -T $(LF_SOURCE_GEN_DIRECTORY)/../../flexpret/programs/lib/linker/flexpret.ld
+RISCV_LINK ?= -T "$(LF_PROJECT_ROOT)/flexpret/programs/lib/linker/flexpret.ld"
 RISCV_LINK_OPTS ?= -Xlinker -Map=output.map
 RISCV_OBJDUMP ?= $(RISCV_PREFIX)objdump
 RISCV_OBJDUMP_OPTS ?= -S -d
 RISCV_OBJCOPY ?= $(RISCV_PREFIX)objcopy
 RISCV_OBJCOPY_OPTS ?= -O binary
 
-INCS  := -I$(LF_SOURCE_GEN_DIRECTORY)/../../flexpret/programs/lib/include \
-    -I$(LF_SOURCE_GEN_DIRECTORY)/include/ \
-    -I$(LF_SOURCE_GEN_DIRECTORY)/include/api \
-    -I$(LF_SOURCE_GEN_DIRECTORY)/include/core \
-    -I$(LF_SOURCE_GEN_DIRECTORY)/include/core/modal_models \
-    -I$(LF_SOURCE_GEN_DIRECTORY)/include/core/utils \
-    -I$(LF_SOURCE_GEN_DIRECTORY)/include/core/platform \
+INCS  := -I"$(LF_PROJECT_ROOT)/flexpret/programs/lib/include" \
+    -I"$(LF_SOURCE_GEN_DIRECTORY)/include/" \
+    -I"$(LF_SOURCE_GEN_DIRECTORY)/include/api" \
+    -I"$(LF_SOURCE_GEN_DIRECTORY)/include/core" \
+    -I"$(LF_SOURCE_GEN_DIRECTORY)/include/core/modal_models" \
+    -I"$(LF_SOURCE_GEN_DIRECTORY)/include/core/utils" \
+    -I"$(LF_SOURCE_GEN_DIRECTORY)/include/core/platform"
 
 # Usually defined in CMakeLists.txt
 # "-DARDUINO" is a hacky way to disable signal() in reactor_common.c.
@@ -70,19 +71,19 @@ $(LF_FILENAME).riscv:
 	# Compile the program into riscv binary.
 	$(RISCV_GCC) $(INCS) $(DEFS) $(RISCV_GCC_OPTS) $(RISCV_LINK) \
 		$(RISCV_LINK_OPTS) -o $@ \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/start.S \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/syscalls.c \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/tinyalloc/tinyalloc.c \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/startup.c \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/platform/lf_flexpret_support.c \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/port.c \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/tag.c \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/utils/pqueue.c \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/utils/util.c \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/utils/vector.c \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/reactor_common.c \
-		$(LF_SOURCE_GEN_DIRECTORY)/core/reactor.c \
-		$(wildcard $(LF_SOURCE_GEN_DIRECTORY)/*.c)
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/start.S" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/syscalls.c" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/tinyalloc/tinyalloc.c" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/startup.c" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/platform/lf_flexpret_support.c" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/port.c" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/tag.c" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/utils/pqueue.c" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/utils/util.c" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/utils/vector.c" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/reactor_common.c" \
+		"$(LF_SOURCE_GEN_DIRECTORY)/core/reactor.c" \
+		$(wildcard $(LF_SOURCE_GEN_DIRECTORY)/*.c) # FIXME: Does not handle spaces.
 
 $(LF_FILENAME).dump: $(LF_FILENAME).riscv
 	$(RISCV_OBJDUMP) $(RISCV_OBJDUMP_OPTS) $< > $@
@@ -110,11 +111,11 @@ cleanall: clean
 
 .PHONY: clean cleanall
 
-' $LF_SOURCE_GEN_DIRECTORY $LF_FILENAME > $LF_SOURCE_GEN_DIRECTORY/Makefile
+' "$PROJECT_ROOT" "$LF_SOURCE_GEN_DIRECTORY" "$LF_FILENAME" > "$LF_SOURCE_GEN_DIRECTORY/Makefile"
 
 echo "Created $LF_SOURCE_GEN_DIRECTORY/Makefile"
 
-cd $LF_SOURCE_GEN_DIRECTORY
+cd "$LF_SOURCE_GEN_DIRECTORY"
 make
 
 echo ""
